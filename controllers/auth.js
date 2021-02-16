180f99349b19218f2af57a3b40d63e568a6fad49
ronbp <ronmagpantay9513@gmail.com>
2021-02-16 14:12:08 -0800

    controllers added and several directories were made

---
+++ controllers/auth.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const passport = require('../config/ppConfig');
+const router = express.Router(); // same as localhost
+
+// import db
+const db = require('../models')
