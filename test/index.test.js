545767658c45c3a10bbae1580f30a79af0dc0216
ronbp <ronmagpantay9513@gmail.com>
2021-02-16 09:58:11 -0800

    Deleted most of the files to start from scratch to get reps in for building out routes, using sequelize, etc.

--- test/index.test.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var request = require('supertest');
-var app = require('../server');
-
-describe('App', function() {
-  it('should return a 200 response', function(done) {
-    request(app).get('/').expect(200, done);
-  });
-});
